d851a83ebecc8b81856e4f00aa5ff1c02418934e
Vasic2000 <Vasic2000@yandex.ru>
2019-11-18 14:03:16 +0300

    Закрываю клавиатуру после выбора города

--- app/src/main/java/ru/vasic2000/newweather/activities/MainActivity.java
+++ app/src/main/java/ru/vasic2000/newweather/activities/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     public void fragmentBack() {
-        drawer.closeDrawers();
         navController.popBackStack();
     }
 
--- app/src/main/java/ru/vasic2000/newweather/fragments/ChangeCity.java
+++ app/src/main/java/ru/vasic2000/newweather/fragments/ChangeCity.java
@@ -1,9 +1,12 @@
 package ru.vasic2000.newweather.fragments;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,28 +22,40 @@ public class ChangeCity extends Fragment {
                              Bundle savedInstanceState) {
         View viewChangeCity = inflater.inflate(R.layout.fragment_change_sity, container, false);
         final EditText city_field = viewChangeCity.findViewById(R.id.et_city_field);
-        Button changeCity = viewChangeCity.findViewById(R.id.btn_changeCity);
+        final Button changeCity = viewChangeCity.findViewById(R.id.btn_changeCity);
 
         changeCity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MainActivity activity = (MainActivity) getActivity();
                 String newCity = city_field.getText().toString();
+                hideKeyboard(getActivity());
                 activity.reDraw(newCity);
                 activity.fragmentBack();
             }
         });
 
-        Button goBack = viewChangeCity.findViewById(R.id.btn_return);
+        final Button goBack = viewChangeCity.findViewById(R.id.btn_return);
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MainActivity activity = (MainActivity) getActivity();
-
+                hideKeyboard(getActivity());
                 activity.fragmentBack();
             }
         });
 
         return viewChangeCity;
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
